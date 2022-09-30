cae57e937af36519bd8e3da51dee638168dfdcdb
Bruno Santos <brunolsantos152@gmail.com>
2022-09-29 23:45:46 -0300

    Update scalablePool.sol

--- scalablePool.sol
+++ scalablePool.sol
@@ -10,20 +10,23 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 //  is ERC721URIStorage, Ownable
-contract lending is ERC721URIStorage, Ownable {
+contract lending is Ownable {
 
     using Counters for Counters.Counter;
     Counters.Counter public _loansIds;
 
     IERC20 public USDCAddress;
     address public poolBorrower;
+    address public portfolioMulti;
+    address public scalablePool;
 
     bool public contractEnabled;
     bool public poolEnabled;
+
     
     //**Pool structure**:
 
-    address public borrower;
+
     //Fixed interest rate
     uint256 public interestRate;
     //Frequency of interest and principal payments, e.g. every 30 days.
@@ -38,19 +41,22 @@ contract lending is ERC721URIStorage, Ownable {
     uint256 public withdrawnAmount;
     //The pool USDC limit amount:
     uint256 public poolLimit;
+    //Total invested to the private investors pool:
+    uint256 public privateInvestorsPoolAmount;
 
 
-    constructor(address _USDCAddress, address _poolBorrower, address _borrower, uint256 _interestRate, uint256 _paymentFrequency, uint256 _term, uint256 _lateFee, uint256 _poolLimit) ERC721("SCALABLE LOAN POOL", "SLP") {
+    constructor(address _USDCAddress, address _poolBorrower, uint256 _interestRate, uint256 _paymentFrequency, uint256 _term, uint256 _lateFee, uint256 _poolLimit, address _portfolioMulti, address _scalablePool)  {
         USDCAddress = IERC20(_USDCAddress);
         poolBorrower = _poolBorrower;
         contractEnabled = true;
         poolEnabled = true;
-        borrower = _borrower;
         interestRate = _interestRate;
         paymentFrequency = _paymentFrequency;
         term = _term;
         lateFee = _lateFee;
         poolLimit = _poolLimit;    
+        portfolioMulti = _portfolioMulti;
+        scalablePool = _scalablePool;
     }
 
 
@@ -92,6 +98,7 @@ contract lending is ERC721URIStorage, Ownable {
         uint256 withdrawnAmount;
         uint256 withdrawnTimestamp;
         uint32 repaidLoansAmount;
+        uint256 repaidLoansValue;
     }
 
 
@@ -102,13 +109,18 @@ contract lending is ERC721URIStorage, Ownable {
 
     //allow the borrower to get the credit from the pool:
     function withdrawFromPool(uint256 _withdrawnAmount) public onlyBorrower() onlyContractEnabled() {
-
+        
+        bool sent = USDCAddress.transfer(msg.sender, _withdrawnAmount);
+        require(sent, "Failed to withdraw the loan");
         _loansIds.increment();
 
         Loans[_loansIds.current()] = Loan ({
             withdrawnAmount: _withdrawnAmount,
             withdrawnTimestamp: block.timestamp,
-            repaidLoansAmount: 0
+            //quantas parcelar já foram pagas:
+            repaidLoansAmount: 0,
+            //o valor total do montante pago:
+            repaidLoansValue: 0
         });
 
         emit loanWithdrawn(_withdrawnAmount, block.timestamp);
@@ -126,18 +138,59 @@ contract lending is ERC721URIStorage, Ownable {
         if((Loans[_id].withdrawnTimestamp + (((term/paymentFrequency) * 86400) * Loans[_id].repaidLoansAmount) <= block.timestamp)) {
                 uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate) / paymentFrequency));
                 require(USDCAmount >= amount);
-                bool sent = USDCAddress.transferFrom(msg.sender, address(this), USDCAmount);
+                uint256 scalablePoolAmount = USDCAmount * 1/10;
+                bool sent = USDCAddress.transferFrom(msg.sender, scalablePool, scalablePoolAmount);
                 require(sent, "Failed to repay the loan");
+
+
+                //calculando a parte para os privateinvestor:
+                uint256 privateInvestorAmount = USDCAmount * 375/1000;
+                for (uint i = 1; i <= _lendingsIds.current(); i++){
+                    uint256 fee = (Lendings[i].USDCAmount / privateInvestorAmount);
+                    uint256 USDCFee = privateInvestorAmount * fee;
+                    bool sentPrivateInvestorAmount = USDCAddress.transferFrom(msg.sender, Lendings[i].lender, USDCFee);
+                    require(sentPrivateInvestorAmount, "Failed to repay the loan");
+                }
+
+                uint256 portfolioMultiAmount = USDCAmount * 525/1000;
+                bool sentPortfolioMultiAmount = USDCAddress.transferFrom(msg.sender, portfolioMulti, portfolioMultiAmount);
+                require(sentPortfolioMultiAmount, "Failed to repay the loan");
+
+
                 Loans[_id].repaidLoansAmount += 1;
+                Loans[_id].repaidLoansValue += USDCAmount;
+
                 emit loanRepaid(_id, Loans[_id].repaidLoansAmount);
             }
         
         else if((Loans[_id].withdrawnTimestamp + (((term/paymentFrequency) * 86400) * Loans[_id].repaidLoansAmount) > block.timestamp)) {
                 uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate) / paymentFrequency) + ((Loans[_id].withdrawnAmount / paymentFrequency) * (lateFee) * ((Loans[_id].withdrawnTimestamp + (((term/paymentFrequency) * 86400) * Loans[_id].repaidLoansAmount) - block.timestamp))));
                 require(USDCAmount >= amount);
-                bool sent = USDCAddress.transferFrom(msg.sender, address(this), USDCAmount);
+                uint256 scalablePoolAmount = USDCAmount * 1/10;
+                bool sent = USDCAddress.transferFrom(msg.sender, scalablePool, scalablePoolAmount);
                 require(sent, "Failed to repay the loan");
+
+                                //calculando a parte para os privateinvestor:
+                uint256 privateInvestorAmount = USDCAmount * 375/1000;
+                for (uint i = 1; i <= _lendingsIds.current(); i++){
+                    uint256 fee = (Lendings[i].USDCAmount / privateInvestorAmount);
+                    uint256 USDCFee = USDCAmount * fee;
+                    bool sentPrivateInvestorAmount = USDCAddress.transferFrom(msg.sender, Lendings[i].lender, USDCFee);
+                    require(sentPrivateInvestorAmount, "Failed to repay the loan");
+                }
+
+                uint256 portfolioMultiAmount = USDCAmount * 525/1000;
+                bool sentPortfolioMultiAmount = USDCAddress.transferFrom(msg.sender, portfolioMulti, portfolioMultiAmount);
+                require(sentPortfolioMultiAmount, "Failed to repay the loan");
+
+
+
+
+
+
+
                 Loans[_id].repaidLoansAmount += 1;
+                Loans[_id].repaidLoansValue += amount;
                 emit loanRepaid(_id, Loans[_id].repaidLoansAmount);
         }
 
@@ -146,9 +199,61 @@ contract lending is ERC721URIStorage, Ownable {
         }
 
 
+        function teste(uint256 _id, uint256 USDCAmount) public {
+        require(Loans[_id].repaidLoansAmount < paymentFrequency, "erro aqui");
+
+        if((Loans[_id].withdrawnTimestamp + (((term/paymentFrequency) * 86400) * Loans[_id].repaidLoansAmount) <= block.timestamp)) {
+                uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate) / paymentFrequency));
+                require(USDCAmount >= amount);
+                uint256 scalablePoolAmount = USDCAmount * 1/10;
+                bool sent = USDCAddress.transferFrom(msg.sender, scalablePool, scalablePoolAmount);
+                require(sent, "Failed to repay the loan");
+
+
+                //calculando a parte para os privateinvestor:
+                uint256 privateInvestorAmount = USDCAmount * 375/1000;
+                for (uint i = 1; i <= _lendingsIds.current(); i++){
+                    uint256 fee = (Lendings[i].USDCAmount / privateInvestorAmount);
+                    uint256 USDCFee = privateInvestorAmount * fee;
+                    bool sentPrivateInvestorAmount = USDCAddress.transferFrom(msg.sender, Lendings[i].lender, USDCFee);
+                    require(sentPrivateInvestorAmount, "Failed to repay the loan");
+                }
+
+                uint256 portfolioMultiAmount = USDCAmount * 525/1000;
+                bool sentPortfolioMultiAmount = USDCAddress.transferFrom(msg.sender, portfolioMulti, portfolioMultiAmount);
+                require(sentPortfolioMultiAmount, "Failed to repay the loan");
+
+
+                Loans[_id].repaidLoansAmount += 1;
+                Loans[_id].repaidLoansValue += USDCAmount;
+
+                emit loanRepaid(_id, Loans[_id].repaidLoansAmount);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     struct Lending {
         uint256 lendingTimeStamp;
+        address lender;
         uint256 USDCAmount;
         //mapping -> passa o id da loan e recebe quantos payments ele já recebeu da loan:
         mapping(uint256 => uint256) paymentsReceveid;
@@ -156,6 +261,9 @@ contract lending is ERC721URIStorage, Ownable {
 
     mapping(uint256 => Lending) public Lendings;
     event lendingMinted(uint256 id,  uint256 lendingTimeStamp, uint256 USDCAmount, address lender);
+
+    //mapping to verify if address is a private investor
+    mapping(address => bool) isPrivateInvestor;
 
     Counters.Counter public _lendingsIds;
 
@@ -172,12 +280,25 @@ contract lending is ERC721URIStorage, Ownable {
 
         Lending storage l = Lendings[lIds];
         l.lendingTimeStamp = block.timestamp;
+        l.lender = msg.sender;
         l.USDCAmount = _USDCAmount;
         poolAmount += _USDCAmount;
-
-
+        privateInvestorsPoolAmount += _USDCAmount;
+        isPrivateInvestor[msg.sender] = true;
     }
+
+   /* 
+    function getLoanpayment(uint256 _id) public onlyContractEnabled() {
+        require(isPrivateInvestor[msg.sender] == true);
+        require(Lendings[msg.sender].paymentsReceveid[_id] < Loans[_id].repaidLoansAmount);
+
+        uint256 amount = Loans[_id].repaidLoansAmount - Lendings[msg.sender].paymentsReceveid[_id];
+
+        for (uint256 i = 1; i <= amount; i++) { 
     
+        }
+    }
+    */
     
 
     
