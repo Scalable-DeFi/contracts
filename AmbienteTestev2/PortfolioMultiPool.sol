fdca7a70a750574c2266436ca115748082648f5e
Bruno Santos <brunolsantos152@gmail.com>
2022-10-05 07:27:29 -0300

    Update PortfolioMultiPool.sol

--- AmbienteTestev2/PortfolioMultiPool.sol
+++ AmbienteTestev2/PortfolioMultiPool.sol
@@ -1,25 +1,41 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.7;
 
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-//DÚVIDA: Essa pool emprestou dia 11/08 uma quantidade de dinheiro para um borrower, dia 12/08 uma pessoa X investe nessa pool. Quando a pool recebor o pagamento do borrower, elas distribuirá também
-//para a pessoa X? mesmo se o empréstimo foi feito antes da pessoa X entrar na pool?
+
+interface PortfolioMultiPool {
+    function distribute(uint256) external;
+}
 
 
-contract PortfolioMultiPool is Ownable, ReentrancyGuard{
+/*Este é o contrato do empréstimo em si. Toda vez que um borrower quiser abrir uma ordem de empréstimos, deverá ser feito um
+deploy deste contrato, inserindo as variáveis específicas no constructor após a negociação com o borrower.
+Aqui o private investor poderá investir, bem como a multiportfoliopool. Após a angariação de fundos e abertura da pool,
+o borrower poderá sacar os empréstimos. As parcelas dos empréstimos com o juros acrescentados também será paga por esse contrato,
+que distribuirá proporcionalmente para cada investidor.
+*/
+
+//  is ERC721URIStorage, Ownable
+contract MasterLending is Ownable, ReentrancyGuard{
 
     using Counters for Counters.Counter;
-    Counters.Counter public _investors;
+    Counters.Counter public _loansIds;
 
+    //Pools e endereços:
     IERC20 public USDCAddress;
     address public poolBorrower;
     address public portfolioMulti;
     address public scalablePool;
+    PortfolioMultiPool public portfolioMultiInterface;
 
     bool public contractEnabled;
     bool public poolEnabled;
@@ -28,28 +44,53 @@ contract PortfolioMultiPool is Ownable, ReentrancyGuard{
     //**Pool structure**:
 
 
-
-    //The current amount of USDC in the pool:
+    //Juros
+    uint256 public interestRate;
+    //Pagamanto adicional quando parcelas estão atrasadas (por dia), e.g. 5%. 
+    uint256 public lateFee;
+    //O montante atual de USDC na pool:
     uint256 public poolAmount;
-    //Minimun investment allowed to the pool:
-    uint256 public minInvestment;
-    //The current withdrawn amount from the borrower:
+    //O quanto das pool o borrower já sacou:
     uint256 public withdrawnAmount;
-    //The pool USDC limit amount:
+    //O limite total de USDC que a pool aceita:
     uint256 public poolLimit;
-    //Total invested to the private investors pool:
+    //Total investido pelos private investors:
     uint256 public privateInvestorsPoolAmount;
+    //A porcentagem do lucro que a scalable irá receber:
+    uint256 public scalablePoolFee;
+    //A porcentagem do lucro que os private investors irão receber:
+    uint256 public privateInvestorPoolFee;
+    //A porcentagem do lucro que o portfolioMulti irá receber:
+    uint256 public portfolioMultiPoolFee;
+    //A partir do momento em que o borrower tomar um empréstimo, de quantos em quantos dias ele terá que pagar as parcelas?
+    uint256 public daysDue;
+    //Quantidade total de parcelas a serem pagas:
+    uint256 public paymentFrequency;
 
 
-    constructor(address _USDCAddress, address _poolBorrower, uint256 _minInvestment, uint256 _poolLimit, address _portfolioMulti, address _scalablePool)  {
+    constructor(address _USDCAddress, address _poolBorrower, uint256 _interestRate, uint256 _paymentFrequency, uint256 _lateFee, uint256 _poolLimit, address _portfolioMulti, address _scalablePool, uint256 _scalablePoolFee, uint256 _privateInvestorPoolFee, uint256 _portfolioMultiPoolFee, uint256 _daysDue)  {
+        require(_scalablePoolFee + _privateInvestorPoolFee + _portfolioMultiPoolFee == 1000, "The fees distribution are not correct");
         USDCAddress = IERC20(_USDCAddress);
         poolBorrower = _poolBorrower;
         contractEnabled = true;
         poolEnabled = true;
+        interestRate = _interestRate;
+        paymentFrequency = _paymentFrequency;
+        lateFee = _lateFee;
         poolLimit = _poolLimit;    
         portfolioMulti = _portfolioMulti;
         scalablePool = _scalablePool;
-        minInvestment = _minInvestment;
+        scalablePoolFee = _scalablePoolFee;
+        privateInvestorPoolFee = _privateInvestorPoolFee;
+        portfolioMultiPoolFee = _portfolioMultiPoolFee;
+        daysDue = _daysDue;
+    }
+
+
+
+    //Setar o endereço do contrato do portfolio multi
+    function setPortfolioMultiInterface(PortfolioMultiPool _portfolioMultiInterface) public onlyOwner() {
+        portfolioMultiInterface = _portfolioMultiInterface;
     }
 
 
@@ -68,6 +109,12 @@ contract PortfolioMultiPool is Ownable, ReentrancyGuard{
     }
 
 
+    modifier onlyBorrower() {
+        require(msg.sender == poolBorrower, "Only the pool Borrower can withdraw");
+        _;
+    }
+
+
     function modifyContractEnabled(bool _bool) public onlyOwner() {
         contractEnabled = _bool;
     }
@@ -81,122 +128,177 @@ contract PortfolioMultiPool is Ownable, ReentrancyGuard{
     }
 
 
-
-
-    struct Investor {
-        address investor;
-        uint256 totalAmountInvested;
-        uint256 totalAmountReceived;
-        uint256 totalAmountWithdrawed;
-        bool isActive;
+    //Toda vez que o borrower sacar um montante, criaremos uma "Loan" que armazenará o quanto ele sacou, quando e quanto ele 
+    //já pagou.
+    struct Loan {
+        uint256 withdrawnAmount;
+        uint256 withdrawnTimestamp;
+        uint32 repaidLoansAmount;
+        uint256 repaidLoansValue;
     }
 
-    event newInvestorCreated(address Investor, uint256 totalAmountInvested, uint256 id);
-    event newInvestment(address Investor, uint256 newAmount, uint256 id);
-    event newAmountReceived(address Investor, uint256 amount, uint256 id);
 
-    mapping(uint256 => Investor) public Investors;
-    mapping(address => uint256) public addressToInvestorId;
+    //id to loan
+    mapping(uint256 => Loan) public Loans;
 
-    function investInPool(uint256 _USDCAmount) public onlyPoolEnabled() onlyContractEnabled() {
-        require(_USDCAmount >= minInvestment, "You need to invest more");
+    //eventos para o frontend:
+    event loanWithdrawn(uint256 withdrawnAmount, uint256 withdrawnTimestamp);
+    event loanRepaid(uint256 loanId, uint32 repaidLoansAmount);
 
-
-        //se investidor não existe ainda, criar struct:
-        if(!Investors[addressToInvestorId[msg.sender]].isActive){
-
-            bool sent = USDCAddress.transferFrom(msg.sender, address(this), _USDCAmount);
-            require(sent, "Failed to transfer the amount");
-
-            poolAmount += _USDCAmount;
-            _investors.increment();
-            Investors[_investors.current()] = Investor ({
-                investor: msg.sender,
-                totalAmountInvested: _USDCAmount,
-                totalAmountReceived: 0,
-                totalAmountWithdrawed: 0,
-                isActive: true
-            });
-            addressToInvestorId[msg.sender] = _investors.current();
-            emit newInvestorCreated(msg.sender, _USDCAmount, _investors.current());
-
-        }
-
-        //se o investidor já existe, usar a sua struct:
-        else {
-
-            bool sent = USDCAddress.transferFrom(msg.sender, address(this), _USDCAmount);
-            require(sent, "Failed to transfer the amount");
-
-            poolAmount += _USDCAmount;
-            Investors[addressToInvestorId[msg.sender]].totalAmountInvested += _USDCAmount;
-            emit newInvestment(msg.sender, _USDCAmount, addressToInvestorId[msg.sender]);
-
-        }
+    //Função que permite o borrower sacar da pool:
+    function withdrawFromPool(uint256 _withdrawnAmount) public onlyBorrower() onlyContractEnabled() nonReentrant {
         
+        bool sent = USDCAddress.transfer(msg.sender, _withdrawnAmount);
+        require(sent, "Failed to withdraw the loan");
+        _loansIds.increment();
 
+        Loans[_loansIds.current()] = Loan ({
+            withdrawnAmount: _withdrawnAmount,
+            withdrawnTimestamp: block.timestamp,
+            //quantas parcelar já foram pagas:
+            repaidLoansAmount: 1,
+            //o valor total do montante pago:
+            repaidLoansValue: 0
+        });
+
+        poolAmount -= _withdrawnAmount;
+
+        emit loanWithdrawn(_withdrawnAmount, block.timestamp);
     }
 
-    //fazer um mapping de contratos das pool borrowers, informando se eles fizeram pagamento ou não:
-    mapping(address => bool) public isMasterPool;
-
-    function modifyIsMasterPool(bool _bool, address _address) public onlyOwner() {
-        isMasterPool[_address] = _bool;
+    //return an specific loan created by the borrower
+    function getLoan(uint256 _id) public view returns(Loan memory){
+        return(Loans[_id]);
     }
 
-    //função para distribuir quanto cada um possui de reward para retirar:
-    function distribute(uint256 _USDCAmount) public  {
-        require(isMasterPool[msg.sender], "You are not a verified master pool");
 
-        for (uint256 i = 1; i <= _investors.current(); i++){
-            if(Investors[i].isActive){
-                uint256 amount = _USDCAmount * Investors[i].totalAmountInvested / poolAmount;
-                Investors[i].totalAmountReceived += amount;
-                emit newAmountReceived(Investors[i].investor, amount, i);
+    //Funções para verificar o quanto deverá ser pago e quanto será pago caso atrase os pagamentos:
+
+    function howMuchShouldIPay(uint256 _id) public view returns(uint256) {
+        uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate / 1000) / paymentFrequency));
+        return(amount);
+    }
+
+    function howMuchShouldIPayLate(uint256 _id, uint256 whenYouShouldHavePaid) public view returns(uint256) {
+        //uint256 whenYouShouldHavePaid = (Loans[_id].withdrawnTimestamp + ((30 * 86400) * Loans[_id].repaidLoansAmount));
+        uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate / 1000) / paymentFrequency) + (((Loans[_id].withdrawnAmount * lateFee / 1000) * (block.timestamp - whenYouShouldHavePaid)) / 86400));
+        return(amount);
+    }
+
+    function timeStamp() public view returns(uint256) {
+        return(block.timestamp);
+    }
+
+
+    //Função para repagar uma parcela da Loan:
+    function repayLoan(uint256 _id, uint256 USDCAmount) public onlyContractEnabled() nonReentrant {
+        require(Loans[_id].repaidLoansAmount < paymentFrequency + 1, "error with paymentFrequency");
+
+        if((Loans[_id].withdrawnTimestamp + (((daysDue) * 86400) * Loans[_id].repaidLoansAmount) >= block.timestamp)) {
+                uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate / 1000) / paymentFrequency));
+                require(USDCAmount >= amount, "amount not sufficient");
+                uint256 scalablePoolAmount = USDCAmount * scalablePoolFee / 1000;
+                bool sent = USDCAddress.transferFrom(msg.sender, scalablePool, scalablePoolAmount);
+                require(sent, "Failed to repay the loan");
+
+
+                //calculando a parte para os privateinvestor:
+                uint256 privateInvestorAmount = USDCAmount * privateInvestorPoolFee / 1000;
+                for (uint i = 1; i <= _lendingsIds.current(); i++){
+                    uint256 fee = (Lendings[i].USDCAmount / privateInvestorAmount);
+                    uint256 USDCFee = privateInvestorAmount * fee;
+                    bool sentPrivateInvestorAmount = USDCAddress.transferFrom(msg.sender, Lendings[i].lender, USDCFee);
+                    require(sentPrivateInvestorAmount, "Failed to repay the loan");
+                }
+
+                uint256 portfolioMultiAmount = USDCAmount * portfolioMultiPoolFee / 1000;
+                bool sentPortfolioMultiAmount = USDCAddress.transferFrom(msg.sender, portfolioMulti, portfolioMultiAmount);
+                require(sentPortfolioMultiAmount, "Failed to repay the loan");
+
+
+                Loans[_id].repaidLoansAmount += 1;
+                Loans[_id].repaidLoansValue += USDCAmount;
+
+                portfolioMultiInterface.distribute(USDCAmount);
+
+                emit loanRepaid(_id, Loans[_id].repaidLoansAmount);
             }
-        }
-
-    }
-
-
-
-    function withdrawGains(uint256 _USDCAmount) public nonReentrant {
-        require(Investors[addressToInvestorId[msg.sender]].isActive, "Not allowed investor");
-        require(Investors[addressToInvestorId[msg.sender]].totalAmountReceived > Investors[addressToInvestorId[msg.sender]].totalAmountWithdrawed, "You do not have enough money to withdraw");
         
-        Investors[addressToInvestorId[msg.sender]].totalAmountReceived += _USDCAmount;
-        bool sent = USDCAddress.transfer(msg.sender, _USDCAmount);
-        require(sent, "Failed to withdraw the loan");
+        else if((Loans[_id].withdrawnTimestamp + ((daysDue * 86400) * Loans[_id].repaidLoansAmount) < block.timestamp)) {
 
+                uint256 whenYouShouldHavePaid = (Loans[_id].withdrawnTimestamp + ((daysDue * 86400) * Loans[_id].repaidLoansAmount));
 
-    }
+                uint256 amount = ((Loans[_id].withdrawnAmount / paymentFrequency) + ((Loans[_id].withdrawnAmount * interestRate / 1000) / paymentFrequency) + (((Loans[_id].withdrawnAmount * lateFee / 1000) * (block.timestamp - whenYouShouldHavePaid)) / 86400));
+                require(USDCAmount >= amount);
+                uint256 scalablePoolAmount = USDCAmount * scalablePoolFee / 1000;
+                bool sent = USDCAddress.transferFrom(msg.sender, scalablePool, scalablePoolAmount);
+                require(sent, "Failed to repay the loan");
 
+                                //calculando a parte para os privateinvestor:
+                uint256 privateInvestorAmount = USDCAmount * privateInvestorPoolFee / 1000;
+                for (uint i = 1; i <= _lendingsIds.current(); i++){
+                    uint256 fee = (Lendings[i].USDCAmount / privateInvestorAmount);
+                    uint256 USDCFee = privateInvestorAmount * fee;
+                    bool sentPrivateInvestorAmount = USDCAddress.transferFrom(msg.sender, Lendings[i].lender, USDCFee);
+                    require(sentPrivateInvestorAmount, "Failed to repay the loan");
+                }
 
-        //Finalmente, função chamada para repartir os ganhos da pool:
-    /* function distributeGains(uint256 _USDCAmount) public onlyOwner() {
-        require(poolAmount > 0, "The pool does not have usdc to distribute");
+                uint256 portfolioMultiAmount = USDCAmount * portfolioMultiPoolFee / 1000;
+                bool sentPortfolioMultiAmount = USDCAddress.transferFrom(msg.sender, portfolioMulti, portfolioMultiAmount);
+                require(sentPortfolioMultiAmount, "Failed to repay the loan");
 
-        for (uint256 i = 1; i <= _investors.current(); i++){
-            if(Investors[i].isActive){
-                uint256 amount = Investors[i].totalAmountInvested / poolAmount;
+                Loans[_id].repaidLoansAmount += 1;
+                Loans[_id].repaidLoansValue += amount;
 
-                bool sent = USDCAddress.transfer(Investors[i].investor, amount * _USDCAmount);
-                require(sent, "Failed to withdraw the loan");
-                Investors[i].totalAmountReceived += (amount * _USDCAmount);
+                portfolioMultiInterface.distribute(USDCAmount);
 
-            }
+                emit loanRepaid(_id, Loans[_id].repaidLoansAmount);
         }
-    }
-    */
+       
 
-    //Permitir investidor sacar seu dinheiro:
-    //Isso será permitido? Já que pode não ter dinheiro na pool por conta do empréstimo para borrowers
-    /*
-    function withdrawAmount(uint256 _USDCAmount) public onlyContractEnabled() onlyPoolEnabled() {
-        require(_USDCAmount <= Investors[addressToInvestorId[msg.sender]].totalAmountReceived);
-        bool sent = USDCAddress.transfer(msg.sender, _USDCAmount);
-        require(sent, "Failed to withdraw the loan");
+        }
+
+
+    //Toda vez que um privateInvestor fizer um empréstimo na pool, será criado uma lending que armazena seu endereço,
+    //o quanto investiu e quando.
+
+    struct Lending {
+        uint256 lendingTimeStamp;
+        address lender;
+        uint256 USDCAmount;
+        //mapping -> passa o id da loan e recebe quantos payments ele já recebeu da loan:
+        mapping(uint256 => uint256) paymentsReceveid;
     }
-    */
-   
+
+    mapping(uint256 => Lending) public Lendings;
+    event lendingMinted(uint256 id,  uint256 lendingTimeStamp, uint256 USDCAmount, address lender);
+
+    //mapping to verify if address is a private investor
+    mapping(address => bool) isPrivateInvestor;
+
+    Counters.Counter public _lendingsIds;
+
+    //função para os privateinvestors serem capazes de investir na pool:
+    function lendToBorrowerPool(uint256 _USDCAmount) public onlyPoolEnabled() onlyContractEnabled() nonReentrant {
+        require(poolAmount + _USDCAmount <= poolLimit);
+
+        bool sent = USDCAddress.transferFrom(msg.sender, address(this), _USDCAmount);
+        require(sent, "Failed to lend to borrower pool");
+
+        _lendingsIds.increment();
+
+        uint256 lIds = _lendingsIds.current();
+
+        Lending storage l = Lendings[lIds];
+        l.lendingTimeStamp = block.timestamp;
+        l.lender = msg.sender;
+        l.USDCAmount = _USDCAmount;
+        poolAmount += _USDCAmount;
+        privateInvestorsPoolAmount += _USDCAmount;
+        isPrivateInvestor[msg.sender] = true;
+    }
+
+    
+
+    
 }
